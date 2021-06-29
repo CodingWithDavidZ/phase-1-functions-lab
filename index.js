419f0c18f3fdf5d0a85af956bdf80a37ffbeb5bc
David Zombar <davidzombar@gmail.com>
2021-06-29 12:24:03 -0600

    commit tree d7bc8e71b9b8a794ac6928e5f09caa1b0612cd60

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  let distance = Math.abs(blocks - 42);
+  return distance;
+}
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let fareDistance = distanceTravelledInFeet(start, destination);
+  if (fareDistance <= 400) {
+    return 0;
+  } else if (fareDistance > 400 && fareDistance <= 2000) {
+    return 0.02 * (fareDistance - 400);
+  } else if (fareDistance > 2000 && fareDistance < 2500) {
+    return 25;
+  } else return "cannot travel that far";
+}
